87a027782a3fe633aa6b59c23d6cd01cf4f5afb1
sanao1006 <naosak1006@gmail.com>
2024-04-11 17:05:13 +0900

    fix

--- mlbapi/src/main/java/app/sanao1006/mlbapi/client/attendance/AttendanceClientImpl.kt
+++ mlbapi/src/main/java/app/sanao1006/mlbapi/client/attendance/AttendanceClientImpl.kt
@@ -1,19 +1,14 @@
 package app.sanao1006.mlbapi.client.attendance
 
 import app.sanao1006.mlbapi.model.attendance.Attendance
-import app.sanao1006.mlbapi.model.award.Award
-import app.sanao1006.mlbapi.model.award.Awards
-import com.skydoves.sandwich.ApiResponse
 import com.skydoves.sandwich.getOrThrow
-import com.skydoves.sandwich.onSuccess
-import java.net.ConnectException
 
 class AttendanceClientImpl(
     private val attendanceClient: AttendanceClient
 ) {
     suspend fun getAttendance(
-        leagueId: Int,
         teamId: Int,
+        leagueId: Int,
         leagueListId: String
     ): Attendance = attendanceClient.getAttendance(
         teamId = teamId,
