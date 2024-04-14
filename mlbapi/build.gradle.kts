c1bf66b6898e6c9b15650ccb4ca63d907666eb85
sanao1006 <naosak1006@gmail.com>
2024-04-14 19:17:02 +0900

    publishing test 0.0.1

--- mlbapi/build.gradle.kts
+++ mlbapi/build.gradle.kts
@@ -2,12 +2,19 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
-    id("com.google.devtools.ksp") version  "1.9.22-1.0.18"
+    id("com.google.devtools.ksp") version "1.9.22-1.0.18"
     alias(libs.plugins.ktorfit)
+    id("maven-publish")
 }
+
+val GROUP_ID = "com.github.sanao1006"
+val ARTIFACT_ID = "mlb-api-kt"
+val VERSION = "0.0.1"
 
 android {
     namespace = "app.sanao1006.mlbkt"
+    group = GROUP_ID
+    version = VERSION
     compileSdk = 34
 
     defaultConfig {
@@ -47,4 +54,17 @@ dependencies {
     testImplementation(libs.coroutines.test)
     testImplementation(libs.serialization)
     testImplementation(kotlin("test"))
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = GROUP_ID
+                artifactId = ARTIFACT_ID
+                version = VERSION
+            }
+        }
+    }
 }
