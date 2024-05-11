4c52c4d8646504d2326450b1f05e57811d453741
sanao1006 <naosak1006@gmail.com>
2024-05-11 14:23:28 +0900

    create `jobs_umpires` endpoint client

--- mlbapi/src/main/java/app/sanao1006/mlbapi/client/jobs/JobsClient.kt
+++ mlbapi/src/main/java/app/sanao1006/mlbapi/client/jobs/JobsClient.kt
@@ -1,6 +1,7 @@
 package app.sanao1006.mlbapi.client.jobs
 
 import app.sanao1006.mlbapi.model.jobs.JobsResponse
+import app.sanao1006.mlbapi.model.jobs.JobsUmpiresResponse
 import com.skydoves.sandwich.ApiResponse
 import de.jensklingenberg.ktorfit.http.GET
 import de.jensklingenberg.ktorfit.http.Query
@@ -13,4 +14,11 @@ interface JobsClient {
         @Query("date") date: String?,
         @Query("fields") fields: String?
     ): ApiResponse<JobsResponse>
+
+    @GET("jobs/umpires")
+    suspend fun getJobsUmpires(
+        @Query("sportId") sportId: Int?,
+        @Query("date") date: String?,
+        @Query("fields") fields: String?
+    ): ApiResponse<JobsUmpiresResponse>
 }
--- mlbapi/src/main/java/app/sanao1006/mlbapi/client/jobs/JobsClientImpl.kt
+++ mlbapi/src/main/java/app/sanao1006/mlbapi/client/jobs/JobsClientImpl.kt
@@ -2,6 +2,7 @@ package app.sanao1006.mlbapi.client.jobs
 
 import app.sanao1006.mlbapi.model.jobs.JobType
 import app.sanao1006.mlbapi.model.jobs.JobsResponse
+import app.sanao1006.mlbapi.model.jobs.JobsUmpiresResponse
 import app.sanao1006.mlbapi.model.jobs.Roster
 import com.skydoves.sandwich.ApiResponse
 
@@ -21,7 +22,25 @@ class JobsClientImpl(
             fields = fields
         ).toList()
 
+    suspend fun getJobsUmpires(
+        sportId: Int? = null,
+        date: String? = null,
+        fields: String? = null
+    ): List<Roster> =
+        jobsClient.getJobsUmpires(
+            sportId = sportId,
+            date = date,
+            fields = fields
+        ).toList()
+
     private fun ApiResponse<JobsResponse>.toList(): List<Roster> =
+        when (this) {
+            is ApiResponse.Success -> this.data.roster
+            is ApiResponse.Failure -> emptyList()
+        }
+
+    @JvmName("JobsUmpiresResponseToRosterList")
+    private fun ApiResponse<JobsUmpiresResponse>.toList(): List<Roster> =
         when (this) {
             is ApiResponse.Success -> this.data.roster
             is ApiResponse.Failure -> emptyList()
