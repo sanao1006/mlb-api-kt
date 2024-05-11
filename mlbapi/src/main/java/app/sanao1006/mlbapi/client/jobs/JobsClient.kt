a708b01ae3db22f34be8401e487a5aaddba58cdd
sanao1006 <naosak1006@gmail.com>
2024-05-11 14:37:07 +0900

    create `jobs_officialScorers` endpoint client

--- mlbapi/src/main/java/app/sanao1006/mlbapi/client/jobs/JobsClient.kt
+++ mlbapi/src/main/java/app/sanao1006/mlbapi/client/jobs/JobsClient.kt
@@ -1,6 +1,7 @@
 package app.sanao1006.mlbapi.client.jobs
 
 import app.sanao1006.mlbapi.model.jobs.JobsDatacastersResponse
+import app.sanao1006.mlbapi.model.jobs.JobsOfficialScoresResponse
 import app.sanao1006.mlbapi.model.jobs.JobsResponse
 import app.sanao1006.mlbapi.model.jobs.JobsUmpiresResponse
 import com.skydoves.sandwich.ApiResponse
@@ -29,4 +30,10 @@ interface JobsClient {
         @Query("date") date: String?,
         @Query("fields") fields: String?
     ): ApiResponse<JobsDatacastersResponse>
+
+    @GET("jobs/officialScorers")
+    suspend fun getJobsOfficialScores(
+        @Query("timecode") timecode: String?,
+        @Query("fields") fields: String?
+    ): ApiResponse<JobsOfficialScoresResponse>
 }
--- mlbapi/src/main/java/app/sanao1006/mlbapi/client/jobs/JobsClientImpl.kt
+++ mlbapi/src/main/java/app/sanao1006/mlbapi/client/jobs/JobsClientImpl.kt
@@ -2,6 +2,7 @@ package app.sanao1006.mlbapi.client.jobs
 
 import app.sanao1006.mlbapi.model.jobs.JobType
 import app.sanao1006.mlbapi.model.jobs.JobsDatacastersResponse
+import app.sanao1006.mlbapi.model.jobs.JobsOfficialScoresResponse
 import app.sanao1006.mlbapi.model.jobs.JobsResponse
 import app.sanao1006.mlbapi.model.jobs.JobsUmpiresResponse
 import app.sanao1006.mlbapi.model.jobs.Roster
@@ -45,6 +46,15 @@ class JobsClientImpl(
             fields = fields
         ).toList()
 
+    suspend fun getJobsOfficialScores(
+        timecode: String? = null,
+        fields: String? = null
+    ): List<Roster> =
+        jobsClient.getJobsOfficialScores(
+            timecode = timecode,
+            fields = fields
+        ).toList()
+
     private fun ApiResponse<JobsResponse>.toList(): List<Roster> =
         when (this) {
             is ApiResponse.Success -> this.data.roster
@@ -60,6 +70,12 @@ class JobsClientImpl(
 
     @JvmName("JobsDatacastersResponseToRosterList")
     private fun ApiResponse<JobsDatacastersResponse>.toList(): List<Roster> =
+        when (this) {
+            is ApiResponse.Success -> this.data.roster
+            is ApiResponse.Failure -> emptyList()
+        }
+    @JvmName("JobsOfficialScoresResponseToRosterList")
+    private fun ApiResponse<JobsOfficialScoresResponse>.toList(): List<Roster> =
         when (this) {
             is ApiResponse.Success -> this.data.roster
             is ApiResponse.Failure -> emptyList()
